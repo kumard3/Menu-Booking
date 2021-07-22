53081c65883c4785d7c2a9df66c2a3ad62f2b8f9
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2021-07-22 22:55:25 +0530

    Plus Minus Functinality Added

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -2,26 +2,26 @@ import React, { useState } from "react";
 
 const MenuItem = ({ id, menuItems, setMenuItems, name }) => {
   const [itemNumber, setItemNumber] = useState(0);
-  const [toggleCartButton, setToggleCartButton] = useState(true);
+  const [toggle, setToggle] = useState(false);
   console.log(menuItems);
-  function handleDeleteClick(id) {
-    const removeItem = menuItems.filter((menuItem) => {
-      return menuItem.id !== id;
-    });
-    setItemNumber(0);
-    setMenuItems(removeItem);
-    setToggleCartButton(!toggleCartButton);
-  }
+  // function handleDeleteClick(id) {
+  //   const removeItem = menuItems.filter((menuItem) => {
+  //     return menuItem.id !== id;
+  //   });
+  //   setItemNumber(0);
+  //   setMenuItems(removeItem);
+  //   setToggleCartButton(!toggleCartButton);
+  // }
 
   return (
     <>
       <h3>{name}</h3>
-      <input
+      {/* <input
         type="number"
         value={itemNumber}
         onChange={(e) => setItemNumber(e.target.value)}
-      />
-      {toggleCartButton ? (
+      /> */}
+      {/* {toggleCartButton ? (
         <button
           onClick={() => {
             setToggleCartButton(!toggleCartButton);
@@ -34,16 +34,47 @@ const MenuItem = ({ id, menuItems, setMenuItems, name }) => {
         </button>
       ) : (
         <button onClick={() => handleDeleteClick(id)}>Remove from cart</button>
+      )} */}
+      {/* {itemNumber > 0 ? (
+        <div>
+          <button onClick={() => setItemNumber(itemNumber - 1)}> - </button>{" "}
+          {itemNumber}
+          <button onClick={() => setItemNumber(itemNumber + 1)}>+</button>
+        </div>
+      ) : (
+        <button
+          onClick={() => setItemNumber(itemNumber + 1)}
+          className="bg-white">
+          Add +
+        </button>
+      )} */}
+      {itemNumber > 0 ? (
+        <div>
+          {" "}
+          <h3>Qty - </h3>
+          <button onClick={() => setItemNumber(itemNumber - 1)}>-</button>
+          {itemNumber}
+          <button onClick={() => setItemNumber(itemNumber + 1)}>+</button>
+        </div>
+      ) : (
+        <button
+          onClick={() => setItemNumber(itemNumber + 1)}
+          className="bg-white">
+          Add +
+        </button>
       )}
-      {/* <button
-        onClick={() =>
-          setMenuItems([
-            ...menuItems,
-            { name: name, numberOfPlates: itemNumber },
-          ])
-        }>
-        Add to cart
-      </button> */}
+      {itemNumber && (
+        <button
+          onClick={() => {
+            setToggle(true);
+            setMenuItems([
+              ...menuItems,
+              { id: id, name: name, numberOfPlates: itemNumber },
+            ]);
+          }}>
+          {toggle ? "Added" : "Add to order list"}
+        </button>
+      )}
     </>
   );
 };
