2a3be9fd62e279fc04175a8a01976bee5275b50b
kumard3 <kumardeepanshu157200@gmail.com>
2021-07-22 20:56:06 +0530

    ui added

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "../styles/menuitems.scss";
 
 const MenuItem = ({ id, menuItems, setMenuItems, name }) => {
   const [itemNumber, setItemNumber] = useState(0);
@@ -11,24 +12,38 @@ const MenuItem = ({ id, menuItems, setMenuItems, name }) => {
     setMenuItems(removeItem);
     setToggleCartButton(!toggleCartButton);
   }
-
+  const handleClick1 = () => {
+  
+    setItemNumber(itemNumber + 1)
+  }
   return (
-    <>
-      <h3>{name}</h3>
-      <input
-        type="number"
-        value={itemNumber}
-        onChange={(e) => setItemNumber(e.target.value)}
+    <div className="menuitems">
+      <img
+        className="menuitems__img"
+        src="https://www.simplyrecipes.com/thmb/WFAZWL7N-orBG_wnB52Zun4xxhk=/3111x3111/smart/filters:no_upscale()/Simply-Recipes-Grilled-BBQ-Chicken-LEAD-10-03fd9892eaae4ce1a8a3f4c949657cfd.jpg"
+        alt=""
       />
+      <h3>{name}</h3>
+      <div className="menuitems__add">
+        <input
+          className="menuitems__input"
+          type="number"
+          value={itemNumber}
+          onChange={(e) => setItemNumber(e.target.value)}
+        />
+        <button className="menuitems__button" onClick={handleClick1} > add </button>
+      </div>
       {toggleCartButton ? (
         <button
+        className="menuitems__button1"
           onClick={() => {
             setToggleCartButton(!toggleCartButton);
             setMenuItems([
               ...menuItems,
               { id: id, name: name, numberOfPlates: itemNumber },
             ]);
-          }}>
+          }}
+        >
           Add to cart
         </button>
       ) : (
@@ -43,8 +58,7 @@ const MenuItem = ({ id, menuItems, setMenuItems, name }) => {
         }>
         Add to cart
       </button> */}
-    </>
+    </div>
   );
 };
-
 export default MenuItem;
