0935d405f40f63a7658410d2f3c0980c8276afc7
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2021-07-23 00:16:40 +0530

    Plus Minus added

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -2,14 +2,63 @@ import React, { useState } from "react";
 
 const MenuItem = ({ id, menuItems, setMenuItems, name }) => {
   const [itemNumber, setItemNumber] = useState(0);
-  const [toggle, setToggle] = useState(false);
+  // const [toggle, setToggle] = useState(false);
   console.log(menuItems);
+  const handlePlus = () => {
+    let objIndex = menuItems.findIndex((obj) => obj.id === id);
+    let newItem = [...menuItems];
+    let object = { id: id, name: name, numberOfPlates: itemNumber + 1 };
+    newItem[objIndex] = object;
+    setMenuItems(newItem);
+    setItemNumber(itemNumber + 1);
+  };
 
+  function handleDeleteClick(id) {
+    const removeItem = menuItems.filter((menuItem) => {
+      return menuItem.id !== id;
+    });
+    setItemNumber(0);
+    setMenuItems(removeItem);
+  }
+  const handleMinus = () => {
+    if (itemNumber === 1) {
+      handleDeleteClick(id);
+    } else {
+      let objIndex = menuItems.findIndex((obj) => obj.id === id);
+      let newItem = [...menuItems];
+      let object = { id: id, name: name, numberOfPlates: itemNumber - 1 };
+      newItem[objIndex] = object;
+      setMenuItems(newItem);
+      setItemNumber(itemNumber - 1);
+    }
+  };
+
+  const handlePlusButton = () => {
+    // let objIndex = menuItems.findIndex((item) => item.id === id);
+    // let newMenuItems = [...menuItems];
+    // let updatedItem = { id: id, name: name, numberOfPlates: 1 };
+    // newMenuItems[objIndex] = updatedItem;
+    // setMenuItems(newMenuItems);
+
+    // let objIndex = menuItems.findIndex((obj) => obj.id === id);
+
+    //Log object to Console.
+    // console.log("Before update: ", menuItems[objIndex])
+    // let newItem = [...menuItems];
+    //Update object's name property.
+    // let object = { id: id, name: name, numberOfPlates: 1 };
+    // newItem[objIndex] = object;
+
+    //Log object to console again.
+    // setMenuItems(newItem);
+    setItemNumber(1);
+    setMenuItems([...menuItems, { id: id, name: name, numberOfPlates: 1 }]);
+  };
   return (
     <>
       <h3>{name}</h3>
 
-      {itemNumber > 0 ? (
+      {/* {itemNumber > 0 ? (
         <div>
           {" "}
           <h3>Qty - </h3>
@@ -35,7 +84,16 @@ const MenuItem = ({ id, menuItems, setMenuItems, name }) => {
           }}>
           {toggle ? "Added" : "Add to order list"}
         </button>
-      ) : null}
+      ) : null} */}
+      {itemNumber > 0 ? (
+        <div>
+          <button onClick={handleMinus}>-</button>
+          {itemNumber}
+          <button onClick={handlePlus}>+</button>
+        </div>
+      ) : (
+        <button onClick={handlePlusButton}>Add +</button>
+      )}
     </>
   );
 };
