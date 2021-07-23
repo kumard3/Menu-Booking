278850b52d2da784058554636457f2d88ec8ffda
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2021-07-23 10:12:30 +0530

    Started working on admin page

--- src/pages/MenuPage/index.js
+++ src/pages/MenuPage/index.js
@@ -19,10 +19,15 @@ export const MenuPage = ({ table, menuItems, setMenuItems }) => {
 
   const checkMenu = (event) => {
     event.preventDefault();
-    if (menuItems.length > 0 && name !== "") {
-      submitOrder();
-    } else {
+    if (menuItems.length === 0) {
       alert("Order any meal.");
+    } else if (name === "") {
+      alert("Please enter your name.");
+    } else {
+      submitOrder();
+      alert("Your order has been submitted.");
+      setMenuItems([]);
+      setName("");
     }
   };
 
@@ -30,13 +35,15 @@ export const MenuPage = ({ table, menuItems, setMenuItems }) => {
     <>
       <div className="flex flex-col items-center w-screen h-screen">
         <header className="flex justify-center items-center">
-          <h1 className="text-2xl">Table {table}</h1>
+          <h1 className="text-2xl text-white mb-4">Table {table}</h1>
         </header>
-        <section className="flex w-full justify-around items-center  ">
+        <section className="flex w-full justify-around items-center mb-4  ">
           {" "}
-          <label htmlFor="customerName">Name</label>
+          <label htmlFor="customerName" className="text-white text-lg">
+            Name
+          </label>
           <input
-            className="border-blue-500"
+            className="border-blue-500 rounded"
             type="text"
             value={name}
             required
@@ -45,23 +52,25 @@ export const MenuPage = ({ table, menuItems, setMenuItems }) => {
         </section>
         <main className="w-full h-full flex flex-col items-center">
           {" "}
-          <h1 className="text-2xl">Menu</h1>
+          <h1 className="text-2xl text-white">Menu</h1>
           <MenuItem
             id={0}
             menuItems={menuItems}
             setMenuItems={setMenuItems}
             name={"Chicken"}
+            price={100}
           />
           <MenuItem
             id={1}
             menuItems={menuItems}
             setMenuItems={setMenuItems}
             name={"Mutton"}
+            price={150}
           />
         </main>
 
         <button
-          className="w-full h-28 bg-yellow-400 flex items-center justify-center"
+          className="w-full h-28 bg-yellow-400 flex items-center justify-center text-gray-50 text-3xl"
           onClick={checkMenu}>
           Submit Order
         </button>
